02a3ea01f86186a9003d1f252fb0210963e53a8b
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2017-09-10 22:00:38 +0700

    exercises-1

---
+++ exercises-1.js
@@ -0,0 +1,17 @@
+var number1 = 33;
+console.log(number1);
+
+var number2 = 6;
+console.log(number2);
+
+var string1 = "foo";
+console.log(string1);
+
+var string2 = "bar";
+console.log(string2);
+
+var boolean1 = true;
+console.log(boolean1);
+
+var boolean2 = false;
+console.log(boolean2);
