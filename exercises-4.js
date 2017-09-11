58d334bb0bcbe734ab0cb6810e5eacb6eaccc00a
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2017-09-11 23:01:52 +0700

    exercises-4.js

---
+++ exercises-4.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = 20;
+while(a <= 20){
+  console.log(a+" - I love coding");
+  a+=2;
+}
+
+while(b >= 2){
+  console.log(b+" - I will become fullstack developer");
+  b-=2;
+}
+
+//http://jsbin.com/ciwuluj/edit?js,console
